b2edb4d156a3e5a4116afc38c1d88854f8afe7dd
Akkil MG <akkilcharanmg@gmail.com>
2024-12-31 21:22:56 +0530

    Fixes (test)

--- src/components/main/loading.tsx
+++ src/components/main/loading.tsx
@@ -1,30 +1,13 @@
-import { useEffect, useState } from "react";
-import dynamic from 'next/dynamic';
 
-const Player = dynamic(() => import('react-lottie-player'), { ssr: false });
 
+import React from "react";
+import Player from "react-lottie-player";
 import animationData from "../assets/seach_image.json";
 
 const Loading = () => {
-    const [isClient, setIsClient] = useState(false);
-
-    // Ensure the component is only rendered client-side
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
-
-    if (!isClient) {
-        return null; // This avoids rendering the component on SSR
-    }
-
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black z-50">
-            <Player 
-                loop 
-                animationData={animationData} 
-                play 
-                style={{ width: 200, height: 200 }} 
-            />
+            <Player loop animationData={animationData} play style={{ width: 150, height: 150 }} />
         </div>
     );
 };
