6a1dfae1363c3f9c526209f36acd8b6e4403294f
Akkil MG <akkilcharanmg@gmail.com>
2024-12-31 21:17:52 +0530

    Fixes (test)

--- src/components/main/loading.tsx
+++ src/components/main/loading.tsx
@@ -7,7 +7,7 @@ import animationData from "../assets/seach_image.json";
 const Loading = () => {
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black z-50">
-            <Player loop animationData={animationData} play style={{ width: 200, height: 200 }} />
+            <Player loop animationData={animationData} play style={{ width: 150, height: 150 }} />
         </div>
     );
 };
