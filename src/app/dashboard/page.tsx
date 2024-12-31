2a9e7185c941c7db294abc30d202cd97824f2c99
Akkil MG <akkilcharanmg@gmail.com>
2025-01-01 01:28:40 +0530

    Fixes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,23 +3,23 @@
 import Dashboard from "@/components/dashboard/links";
 import Footer from "@/components/main/footer";
 import Header from "@/components/main/header";
-import Loading from "@/components/main/loading";
 import Policy from "@/components/pop/policy";
 import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+
+const Loading = dynamic(() => import("@/components/main/loading"), { ssr: false });
 
 export default function DashboardPage() {
     const [loading, setLoading] = useState(true);
-    const [isClient, setIsClient] = useState(false);
 
     useEffect(() => {
-        setIsClient(true); 
         const timer = setTimeout(() => setLoading(false), 2000);
         return () => clearTimeout(timer);
     }, []);
 
     return (
         <div className="h-screen">
-            {loading && isClient && <Loading />}
+            {loading && <Loading />}
             <Policy />
             <Header />
             <Dashboard />
