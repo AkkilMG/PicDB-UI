12706913f7f1bd4565e9d748ce0d1c131109238a
Akkil MG <akkilcharanmg@gmail.com>
2026-06-23 16:21:13 +0530

    App update

--- src/app/api/app/play-store/route.ts
+++ src/app/api/app/play-store/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   return NextResponse.json({
-    latest_version: "1.3.0",
-    min_required_version: "1.3.0",
+    latest_version: "1.3.1",
+    min_required_version: "1.3.1",
     update_url: "https://play.google.com/store/apps/details?id=com.arkynox.picdb"
   });
 }
